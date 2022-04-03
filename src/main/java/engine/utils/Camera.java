4960cb24f4473d4ea62e05e332136ee58b6b6500
KoosSA <kkposbus@gmail.com>
2022-04-03 15:16:37 +0200

    Added ability to change camera pitch

--- src/main/java/engine/utils/Camera.java
+++ src/main/java/engine/utils/Camera.java
@@ -17,6 +17,7 @@ public class Camera implements IInputHandler {
 	private Matrix4f viewMatrix = new Matrix4f();
 	private Matrix4f projectionMatrix = new Matrix4f();
 	private float fov = 45.0f, nearPlane = 0.001f, farPlane = 1000.0f;
+	private boolean active = true;
 
 	public Camera() {
 		registerInputHandler();
@@ -38,6 +39,10 @@ public class Camera implements IInputHandler {
 		rotation.y = MathUtils.loop(rotation.y() + angle * 0.5f, 0, 360);
 	}
 
+	private void pitch(float angle) {
+		rotation.x = MathUtils.clamp(rotation.x() - angle * 0.5f, -90, 90);
+	}
+
 	public Matrix4f getViewMatrix() {
 		viewMatrix.identity();
 		viewMatrix.rotateXYZ(Math.toRadians(rotation.x), Math.toRadians(rotation.y), Math.toRadians(rotation.z));
@@ -54,7 +59,9 @@ public class Camera implements IInputHandler {
 
 	@Override
 	public void handleInput(float delta, GameInput input) {
+		if (!active) return;
 		turn(input.getMouseDeltaX());
+		pitch(input.getMouseDeltaY());
 		if (input.isKeyDown(KeyBindings.MOVE_FORWARD))
 			moveForward(delta, 1);
 		if (input.isKeyDown(KeyBindings.MOVE_BACK))
@@ -67,6 +74,14 @@ public class Camera implements IInputHandler {
 			move(0, -delta, 0);
 		if (input.isKeyDown(KeyBindings.MOVE_DOWN))
 			move(0, delta, 0);
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public boolean isActive() {
+		return active;
 	}
 
 }
