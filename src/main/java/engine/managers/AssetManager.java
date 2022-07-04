e15b46a8d273fdf858e4c1e2067aa045df5bc71b
KoosSA <kkposbus@gmail.com>
2022-07-04 15:28:00 +0200

    Added list of light to world

--- src/main/java/engine/managers/AssetManager.java
+++ src/main/java/engine/managers/AssetManager.java
@@ -1,18 +1,25 @@
 package engine.managers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import engine.lights.Light;
 import engine.models.Model;
 import engine.models.ModelInstance;
 
 public class AssetManager {
 
 	private static Map<Model, List<ModelInstance>> toRender = new HashMap<Model, List<ModelInstance>>();
+	private static List<Light> lights = new ArrayList<>();
 
 	public static Map<Model, List<ModelInstance>> getModelsToRender() {
 		return toRender;
+	}
+
+	public static List<Light> getLights() {
+		return lights;
 	}
 
 
